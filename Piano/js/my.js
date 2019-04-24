ac1fd17d6faec4c2e678c3a5e90517f3fe664702
VasyaBilak <vasyabilak.vb@gmail.com>
2019-04-24 13:13:28 +0300

    piano

---
+++ Piano/js/my.js
@@ -0,0 +1,54 @@
+'use strict';
+const Cbleep = new Audio()
+Cbleep.src = "C.wav";
+
+const Dbleep = new Audio()
+Dbleep.src = "D.wav";
+
+const Ebleep = new Audio()
+Ebleep.src = "E.wav";
+
+const Fbleep = new Audio()
+Fbleep.src = "F.wav";
+
+const Ableep = new Audio()
+Ableep.src = "A.wav";
+
+const Gbleep = new Audio()
+Gbleep.src = "G.wav";
+
+const Bbleep = new Audio()
+Bbleep.src = "B.wav";
+
+const C1bleep = new Audio()
+C1bleep.src = "C1.wav";
+
+const D1bleep = new Audio()
+D1bleep.src = "D1.wav";
+
+const E1bleep = new Audio()
+E1bleep.src = "E1.wav";
+
+const F1bleep = new Audio()
+F1bleep.src = "F1.wav";
+
+const Fsbleep = new Audio()
+Fsbleep.src = "F_s.wav";
+
+const Csbleep = new Audio()
+Csbleep.src = "C_s.wav";
+
+const Dsbleep = new Audio()
+Dsbleep.src = "D_s.wav";
+
+const Gsbleep = new Audio()
+Gsbleep.src = "G_s.wav";
+
+const Bbbleep = new Audio()
+Bbbleep.src = "Bb.wav";
+
+const Cs1bleep = new Audio()
+Cs1bleep.src = "C_s1.wav";
+
+const Ds1bleep = new Audio()
+Ds1bleep.src = "D_s1.wav";
